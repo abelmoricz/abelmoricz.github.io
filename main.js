d537ceb5332d159bae7ff9a254d5c7a134d7eafb
Abel <abel.moricz@gmail.com>
2020-04-08 08:48:54 +0200

    cleaner setup

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+
+
 var head= document.getElementsByTagName('head')[0];
-   var resize_p5_func_script= document.createElement('resize_p5_func');
-   script.src= 'resize_p5_func.js';
-   head.appendChild(resize_p5_func_script);
+    var resize_p5_func_script= document.createElement('resize_p5_func');
+    script.src= 'resize_p5_func.js';
+    head.appendChild(resize_p5_func_script);
+
+
 
 
 function setup(){
-    var canvas = createCanvas(dw_getWindowDims().width ,dw_getWindowDims().height);
-    canvas.parent('sketch-div');
+    createCanvas(dw_getWindowDims().width ,dw_getWindowDims().height);
     
-    background(32);
+    background(255);
+    
+    
 }
 
 function draw(){
-    fill(255);
-    ellipse(mouseX, mouseY, 25, 25);
+    //menu_x_pos = dw_getWindowDims.width()/3
+/*
+    for (int i = 0; i < 40; i = i+1) {
+
+        line(30, i, 80, i);
+      
+    }
+    */
+    textSize(40);
+    text("Home", dw_getWindowDims().width/3.5, 50);
+    text("About Me", dw_getWindowDims().width/2.5, 50);
+    text("Projects", dw_getWindowDims().width/1.8, 50);
+
+    
 }
