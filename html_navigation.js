2289a7cef82eaf710cc389c7db45f65e8c947ba1
Abel <abel.moricz@gmail.com>
2020-04-12 11:10:34 +0200

    better bounce achieved

--- html_navigation.js
+++ html_navigation.js
@@ -1,32 +1,26 @@
+
 function change_to_p5() {
     var x = document.getElementById("all");
       x.style.display = "none";
-      
-
   } 
-
+function show_html() {
+    document.getElementById("all").style.display = "block";
+}
 
 function about_me() {
-    var about_me = document.getElementById("about_me");
-    var projects = document.getElementById("projects");
-    var AM = document.getElementById("AM");
-    var P = document.getElementById("P");
-
-    AM.style.textDecoration = "underline";
-    P.style.textDecoration = "none";
-    about_me.style.display = "block";
-    projects.style.display = "none";
+    document.getElementById("AM").style.textDecoration = "underline";
+    document.getElementById("P").style.textDecoration = "none";
+    document.getElementById("about_me").style.display = "block";
+    document.getElementById("projects").style.display = "none";
+    setup();
+    //console.log(document.getElementById("about_me").style.display === "block");
     }
 
 function projects() {
-    var about_me = document.getElementById("about_me");
-    var projects = document.getElementById("projects");
-    var AM = document.getElementById("AM");
-    var P = document.getElementById("P");
-
-    AM.style.textDecoration = "none";
-    P.style.textDecoration = "underline";
-    
-    about_me.style.display = "none";
-    projects.style.display = "block";
+    document.getElementById("AM").style.textDecoration = "none";
+    document.getElementById("P").style.textDecoration = "underline";
+    document.getElementById("about_me").style.display = "none";
+    document.getElementById("projects").style.display = "block";
+    setup();
+    //onsole.log(document.getElementById("about_me").style.display === "block");
     }
--- scratch_2.js
+++ scratch_2.js
@@ -11,11 +11,15 @@
 
 var mgr;
 let img;
+let back_button_img;
 let img_arr = [];
-let col_num = 16;
-let row_num = 9;
-let patience = 0;
+let col_num = 9;
+let row_num;
+let row_num_about = 5;
+let row_num_projects = 7;
 
+let side_length = 80;
+let whole_img = 'about_me';
 
 function loadImageErrorOverride(errEvt) {
   const pic = errEvt.target;
@@ -28,26 +32,43 @@ function setup()
 {
     mgr = new SceneManager();
     //preloads images
-    for(let i = 0; i < 144; ++i){
-      img = loadImage('assets/'+i+'.png',function (pic) { print(img = pic), redraw(); },
+    row_num = row_num_about;
+
+    if(document.getElementById("about_me").style.display === "block"){
+      row_num = row_num_about;
+      whole_img = 'about_me'
+    }
+    if(document.getElementById("projects").style.display === "block"){
+      //console.log(document.getElementById("projects").style.display === "block");
+      row_num = row_num_projects;
+      whole_img = 'projects';
+    }
+
+
+    for(let i = 0; i < (col_num*row_num); ++i){
+      img = loadImage('assets/'+ whole_img + '/' + i +'.png',function (pic) { print(img = pic), redraw(); },
     loadImageErrorOverride);
       img_arr[i] = img;
     }
+
+    //back button loading
+    back_button_img = loadImage('back_button.png',function (pic) { print(img = pic), redraw(); },
+    loadImageErrorOverride);
+    
+    // = row_num_projects;
     
 }
 
 function draw()
 {
+    
+
     let html_element = document.getElementById("all");
     mgr.draw();
     
     if(html_element.style.display === "none"){
       mgr.showScene( FallApart );
     }
-    if(patience > 20){
-      window.location.reload(false); 
-    }
-    
 }
 
 function mousePressed(){ mgr.handleEvent("mousePressed"); }
@@ -56,37 +77,35 @@ function mousePressed(){ mgr.handleEvent("mousePressed"); }
 
 
 function FallApart(){
-  let numBalls = 144; //144
   let balls = [];
-  let friction = 0.999;  //ideal friction between 0.999(almost none) to 0.9(very heavy)
-  let stop_border = 0.00001;
   
-  
-
     this.setup = function() {
       createCanvas(windowWidth, windowHeight);
       
       //display images
       let pic_counter = 0;
-      let x_offset = (windowWidth*.5)-324;
-      let y_offset = 60;
+      let x_offset = (windowWidth*.5)-366;
+      let y_offset =39;
 
     for(let i = 0; i < row_num; ++i){
       for (let j = 0; j < col_num; ++j){
-        balls[pic_counter] = new Ball(x_offset+(j*40), y_offset+(40*i), 40, pic_counter, balls, img_arr[pic_counter]);
+        balls[pic_counter] = new Ball(x_offset+(j*side_length), y_offset + (side_length*i), side_length, pic_counter, balls, img_arr[pic_counter], true);
         pic_counter += 1;
       }
     }
+    balls[pic_counter] = new Ball(width-side_length, 0, side_length,pic_counter, balls, back_button_img, false)
       noStroke();
     }
   
     this.draw = function() {
       background(255);
+      fill(100);
+      
+      
       balls.forEach (ball => {
         ball.display();
         ball.collide();
         ball.move();
-        //ball.cursor();
       });
 
     }
@@ -95,6 +114,7 @@ function FallApart(){
       balls.forEach (ball => {
         ball.clicked();
       })
+      balls[(balls.length)-1].go_back();
     }
   
   // ==========================================================================
@@ -102,26 +122,25 @@ function FallApart(){
   class Ball {
   
     // the constructor
-    constructor(xin, yin, din, idin, oin, text_img_in) {
+    constructor(xin, yin, din, idin, oin, text_img_in, movable_in) {
       this.x = xin;
       this.original_x = xin;
       this.y = yin;
       this.original_y = yin;
-      this.vx =random(-5,5);
-      this.vy = random(-5,5);
+      this.vx = random(-2,2);
+      this.vy = random(-2,2);
       this.diameter = din;
       this.id = idin;
       this.others = oin;
       this.pic = text_img_in;
       this.stroke_weight = 1;
-      this.movable = true;
+      this.movable = movable_in;
 
     }
   
     collide() {
 
-      
-      for (let i = 0; i < numBalls; i++) {
+      for (let i = 0; i < balls.length; i++) {
   
         let dx = this.others[i].x - this.x;
         let dy = this.others[i].y - this.y;
@@ -165,50 +184,44 @@ function FallApart(){
           top_bounce = false;
           bottom_bounce = true;
         }
+        //LEFT and RIGHT bounce
+        if(distance_x <= side_length && distance_y <= side_length && distance_x != 0 && (right_bounce || left_bounce)){
+          if(right_bounce){ this.x = this.others[i].x - side_length; }
+          if(left_bounce){ this.x = this.others[i].x + side_length; }
           
-        if(distance_x <= 40 && distance_y <= 40 && distance_x != 0 && (right_bounce || left_bounce)){
-          if(right_bounce){
-            this.x = this.others[i].x - 40;
-          }
-          if(left_bounce){
-            this.x = this.others[i].x + 40;
-          }
           let temp = this.vx;
+          if(this.movable && this.others[i].movable){
           this.vx = this.others[i].vx;
           this.others[i].vx = temp;
+          }
+          if(this.movable && !(this.others[i].movable)){
+            this.vx *= -1;
+          }
           
         }
-        if(distance_x <= 40 && distance_y <= 40 && (distance_y > 10) && (top_bounce || bottom_bounce)){
-          if(top_bounce){
-            this.y = this.others[i].y + 40;
-          }
-          if(bottom_bounce){
-            this.y = this.others[i].y -40;
-          }
+        if(distance_x <= side_length && distance_y <= side_length && (distance_y != 0) && (top_bounce || bottom_bounce)){
+          if(top_bounce){ this.y = this.others[i].y + side_length; }
+          if(bottom_bounce){ this.y = this.others[i].y -side_length; }
+          
           let temp = this.vy
+          if(this.movable && this.others[i].movable){
           this.vy = this.others[i].vy; 
           this.others[i].vy = temp;
+          }
+          if(this.movable && !(this.others[i].movable)){
+            this.vy *= -1;
+          }
           
         }//end if
+        if(!this.movable){
+          this.x = this.original_x;
+          this.y = this.original_y;
+        }
+
       }//end for
     }// end collide
   
     move() {
-      if(abs(this.vy) > stop_border){
-        //this.vy = this.vy + gravity;
-      }
-      this.vy *= friction;
-      this.vx *= friction;
-      
-      if(abs(this.vy) < stop_border){
-        this.vy = 0;
-      }
-      if(abs(this.vx) < stop_border){
-        this.vx = 0;
-      }
-
-      
-      
       //console.log(balls[126].vy)
       this.x += this.vx;
       this.y += this.vy;
@@ -224,7 +237,7 @@ function FallApart(){
       if (this.y + this.diameter >= height) {
         this.y = height - this.diameter;
         this.vy = -1 * this.vy;
-      } else if (this.y < 0) {
+      } else if (this.y <= 0) {
         this.y = 0;
         this.vy = abs(this.vy);
       }
@@ -233,23 +246,24 @@ function FallApart(){
     display() {
       image(this.pic, this.x, this.y, this.diameter, this.diameter);
       fill(255,0)
-      stroke(0, 10);
+      stroke(0, 50);
       strokeWeight(this.stroke_weight);      
       rect(this.x, this.y, this.diameter, this.diameter); 
     }
     
     clicked() {
-        if(mouseX < this.x + 40 && mouseX > this.x && mouseY < this.y + 40 && mouseY > this.y){
+        if(mouseX < this.x + side_length && mouseX > this.x && mouseY < this.y + side_length && mouseY > this.y){
           this.vx = 0;
           this.vy = 0;
           this.x = this.original_x;
           this.y = this.original_y;
-          this.stroke_weight = 0;
           this.movable = false; 
-          patience += 1;
         }
       }
-    cursor() {
+    go_back() {
+      if(mouseX < this.x + side_length && mouseX > this.x && mouseY < this.y + side_length && mouseY > this.y){
+        window.location.reload(false);
+      }
       
     }
 
